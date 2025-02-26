041ae3ada234e718abd227223d49b6c8bf109ed3
Alex Zhang <zhangchi0104@live.com>
2025-02-26 23:02:32 +1100

    feat: bootstraped backend and graphql

---
+++ packages/graphql/src/index.spec.ts
@@ -0,0 +1,57 @@
+import { test, expect, describe, beforeAll } from "vitest";
+import { loadTypedefsFromFs } from "./index";
+import { writeFileSync, unlinkSync } from "fs";
+import { join, resolve } from "path";
+const SCHEMA_GRAPHQL_CONTENT = `enum AustralianState {
+  NSW
+  VIC
+  QLD
+  SA
+  WA
+  TAS
+  NT
+}
+type Locality {
+  # id of the locality
+  id: Int!
+  # category of the locality, only "Delivery Area"
+  category: String!
+  # latitude of the locality
+  latitude: Float!
+  # name of the locality
+  location: String!
+  # longitude of the locality
+  longitude: Float!
+  # 4-digit Australian postcode
+  postcode: Int!
+  # state of the locality
+  state: AustralianState!
+}
+
+type Localities {
+  # list of localities, cannot be null or array of nulls
+  localities: [Locality!]!
+}
+type Query {
+  localities: Localities
+}
+`;
+const TEMP_SCHEMA_PATH = "schema.tmp.graphql";
+beforeAll(() => {
+  writeFileSync(TEMP_SCHEMA_PATH, SCHEMA_GRAPHQL_CONTENT);
+  return () => unlinkSync(TEMP_SCHEMA_PATH);
+});
+
+describe("loadTypedefsFromFs", () => {
+  test("should load typedefs from default location", () => {
+    const typedefs = loadTypedefsFromFs();
+    expect(typedefs).toEqual(SCHEMA_GRAPHQL_CONTENT);
+  });
+  test("should load typedefs from non-default location", () => {
+    const typedefs = loadTypedefsFromFs(TEMP_SCHEMA_PATH);
+    expect(typedefs).toEqual(SCHEMA_GRAPHQL_CONTENT);
+  });
+  test("should throw an error if the file does not exist", () => {
+    expect(() => loadTypedefsFromFs("nonexistent.graphql")).toThrow();
+  });
+});
